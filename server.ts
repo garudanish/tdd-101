3db3ba8fca7f6045e9a5e1d1f4fbbd721c081c57
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-12-14 01:16:07 +0900

    Mongoose 이용해 mongodb 연결

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import routes from "./routes";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const GLOBAL_VARIABLES = dotenv.config().parsed || {};
 
 const PORT = 4000;
 
 const app = express();
+mongoose.set("strictQuery", false);
+mongoose.connect(GLOBAL_VARIABLES.MONGO_URI || "");
 
 app.use(express.json());
 
