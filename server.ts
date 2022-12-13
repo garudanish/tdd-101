596ca7135190de5935245b02dd28f617c2231aa5
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-12-14 00:28:59 +0900

    server, routes, controller 설정

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes";
+
+const PORT = 4000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/", routes);
+
+app.listen(PORT);
+
+console.log(`Running on port ${PORT}`);
